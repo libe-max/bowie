ce601e0c36f5edd21658fd7835c510c702e2020b
Maxime Fabas <m.fabas@liberation.fr>
2018-12-19 11:48:13 +0100

    Install libe-components and libe-utils dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PageTitle from 'libe-components/lib/text-levels/PageTitle'
+import parseTsv from 'libe-utils/parse-tsv'
 import * as serviceWorker from './serviceWorker'
 import config from './config.json'
 
 const App = props => <div>
+  <PageTitle>Titre</PageTitle>
   {/* Replace this App component with your app */}
 </div>
 
