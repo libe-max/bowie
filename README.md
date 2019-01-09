b3d1c032d0453c70d84534f2d6e8b7535aef28b5
Maxime Fabas <m.fabas@liberation.fr>
2019-01-09 18:40:18 +0100

    Almost there

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 ## Reste à faire
 
-- Changer et styliser le datepicker
-- Faire le bouton random
-- Ajuster le scroll auto
+OK - Ajuster le scroll auto
+OK - Changer et styliser le datepicker
+OK - Faire le bouton random
+- Changer la side image au scroll
+- Travail sur la page résultats
+OK - Intitulés qui tournent en random
 - Ajouter une intro ?
 - Ajouter les éléments de bas de page (logo, credits, share...)
 - Récupérer les infos habituelles de seo et réseaux
-- Changer la side image au scroll
+- Autoscroll quand click sur single
+- Centrer horiz les éléments au scroll auto
--- src/LibeLaBowie.js
+++ src/LibeLaBowie.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
+import { Parser } from 'html-to-react'
 import YouTube from 'react-youtube'
-import DatePicker from 'react-date-picker'
 import moment from 'moment'
 import 'moment/locale/fr'
 
@@ -8,6 +8,7 @@ import SectionTitle from 'libe-components/lib/text-levels/SectionTitle'
 import Paragraph from 'libe-components/lib/text-levels/Paragraph'
 import squareImg from './assets/square.jpg'
 import closeIcon from './assets/close-icon.svg'
+import randomIcon from './assets/random-icon.svg'
 
 import { parseTsvWithTabs } from 'libe-utils/parse-tsv'
 import { spreadsheet } from './config.json'
@@ -15,6 +16,8 @@ import { spreadsheet } from './config.json'
  * liberation.fr/apps/static/styles/apps.css */
 import './app.css'
 
+import smoothscroll from 'smoothscroll-polyfill'
+smoothscroll.polyfill()
 
 export default class LibeLaBowie extends Component {
   constructor () {
@@ -25,20 +28,29 @@ export default class LibeLaBowie extends Component {
       error: null,
       error_status: null,
       data: null,
-      intro_page: false,
+      intro_page: true,
       active_single_id: null,
-      selected_date: null
+      selected_date: null,
+      active_label: 0,
+      hide_label: false
     }
     this.fetchData = this.fetchData.bind(this)
+    this.h2r = new Parser()
     this.leaveIntro = this.leaveIntro.bind(this)
+    this.handleDateChange = this.handleDateChange.bind(this)
+    this.handleRandomButtonClick = this.handleRandomButtonClick.bind(this)
     this.stretchCoverPanels = this.stretchCoverPanels.bind(this)
+    this.replaceInstruction = this.replaceInstruction.bind(this)
     this.scrollToAndActivateDate = this.scrollToAndActivateDate.bind(this)
+    this.$getClosestParent = this.$getClosestParent.bind(this)
     this.fetchData()
-    window.setInterval(() => this.stretchCoverPanels(), 200)
+    window.setTimeout(() => this.stretchCoverPanels(), 800)
+    window.setInterval(() => this.replaceInstruction(), 4000)
   }
 
   componentWillUnmount () {
-    window.clearInterval(() => this.stretchCoverPanels(), 200)
+    window.clearTimeout(() => this.stretchCoverPanels())
+    window.clearInterval(() => this.replaceInstruction(), 4000)
   }
 
   render () {
@@ -47,12 +59,14 @@ export default class LibeLaBowie extends Component {
     const singles = data ? data.singles : []
     const periods = data ? data.periods : []
     const notes = data ? data.notes : []
+    const labels = data ? data.labels : [{}]
 
     /* Conditional css classes */
     const classes = [c]
     if (state.error) classes.push(`${c}_error`)
     if (state.intro_page) classes.push(`${c}_intro`)
     if (state.active_single_id !== null) classes.push(`${c}_with-player`)
+    if (state.hide_label) classes.push(`${c}_hide-label`)
 
     /* [WIP] render an empty page if data is loading */
     if (state.loading || !state.data) return <div className={classes.join(' ')} />
@@ -64,25 +78,27 @@ export default class LibeLaBowie extends Component {
           style={{backgroundImage: `url(http://www.fubiz.net/wp-content/uploads/2017/08/davidbowie19670.jpg)`}}>
           <div className={`${c}__page-title`}>
             <SectionTitle huge level={1}>
-              Sous quel Bowie<br />suis-je né ?
+              Nos vies<br />sous Bowie
             </SectionTitle>
           </div>
           <div className={`${c}__date-picker-block`}>
-            <span className={`${c}__date-picker`}>
-              <DatePicker value={state.selected_date ? new Date(state.selected_date) : null}
-                onChange={val => this.scrollToAndActivateDate(val.valueOf())} />
-            </span>
+            <div className={`${c}__date-picker`}>
+              <input
+                type='date'
+                ref={n => {this.$datePicker = n}}
+                onChange={this.handleDateChange}
+                defaultValue={moment().format('YYYY-MM-DD')} />
+              <div className={`${c}__date-picker-label`}>
+                {this.h2r.parse(labels[state.active_label].value)}
+              </div>
+            </div>
             <button className={`${c}__random-date`}
-              onClick={this.leaveIntro}>
-              Random
-            </button>
+              onClick={this.handleRandomButtonClick}
+              style={{backgroundImage: `url(${randomIcon})`}} />
           </div>
         </div>
       </div>
       <div className={`${c}__content-panel`}>
-        <div className={`${c}__intro`}>
-          Il faut une intro ici, non ?
-        </div>
         <div className={`${c}__chapters`}>{
           periods.map((period, i) => (
             <section className={`${c}__chapter`} key={i}>
@@ -179,12 +195,17 @@ export default class LibeLaBowie extends Component {
         start: 13,
         end: 13,
         keysLinePos: 1
+      }, {
+        start: 14,
+        end: 14,
+        keysLinePos: 1
       }]
     })
     const res = {
       singles: data[0],
       periods: data[1],
-      notes: data[2]
+      notes: data[2],
+      labels: data[3]
     }
     this.setState({
       loading: false,
@@ -196,34 +217,89 @@ export default class LibeLaBowie extends Component {
   }
 
   leaveIntro () {
-    const { state } = this
-    if (state.intro_page) {
-      this.setState({ intro_page: false })
-    }
+    return new Promise((resolve, reject) => {
+      const { state } = this
+      if (state.intro_page) {
+        this.setState({ intro_page: false })
+        window.setTimeout(a => resolve(true), 200)
+      } else {
+        resolve()  
+      }
+    })
   }
 
   stretchCoverPanels () {
     const { state, c } =  this
     if (state.loading || !state.data) return
-    const bodyPadding = document.querySelector('body').style.paddingTop
-    document.querySelector(`.${c}__cover-panel-wrapper`).style.height = `calc(100vh - ${bodyPadding})`
-    document.querySelector(`.${c}__cover-panel`).style.height = `calc(100vh - ${bodyPadding})`
-    document.querySelectorAll(`.${c}__chapter-name-mobile`).forEach(elt => elt.style.height = `calc(100vh - ${bodyPadding})`)
+    if (!document.querySelector('body')) return
+    if (!document.querySelector(`.${c}__cover-panel-wrapper`)) return
+    if (!document.querySelector(`.${c}__cover-panel`)) return
+    const vh = window.innerHeight
+    const bodyPadding = document.querySelector('body').style.paddingTop || '65px'
+    document.querySelector(`.${c}__cover-panel-wrapper`).style.height = `calc(${vh}px - ${bodyPadding})`
+    document.querySelector(`.${c}__cover-panel`).style.height = `calc(${vh}px - ${bodyPadding})`
+    document.querySelectorAll(`.${c}__chapter-name-mobile`).forEach(elt => elt.style.height = `calc(${vh}px - ${bodyPadding})`)
+  }
+
+  replaceInstruction () {
+    const { state } = this
+    const { data, active_label: activeLabel } = state
+    const { labels } = data
+    return new Promise((resolve, reject) => {
+      this.setState({ hide_label: true })
+      window.setTimeout(() => activeLabel >= labels.length - 1
+        ? this.setState({
+          active_label: 0,
+          hide_label: false
+        })
+        : this.setState({
+          active_label: activeLabel + 1,
+          hide_label: false
+        })
+      , 400)
+    })
+  }
+
+  async handleDateChange (e) {
+    if (!e) return
+    if (!e.target) return
+    const { value } = e.target
+    await this.leaveIntro()
+    const date = moment(value, 'YYYY-MM-DD')
+    if (!moment(date).isValid()) return
+    this.scrollToAndActivateDate(date.valueOf())
+  }
+
+  async handleRandomButtonClick (e) {
+    await this.leaveIntro()
+    const { state } = this
+    const { data } = state
+    const singles = data ? data.singles : []
+    const dates = singles.map(s => s.date.valueOf())
+    const minDate = Math.min(...dates) + 1
+    const maxDate = Math.max(...dates) + 1
+    const timespan = maxDate - minDate
+    const randomDate = Math.random() * timespan + minDate
+    this.$datePicker.value = moment(randomDate, 'x').format('YYYY-MM-DD')
+    this.scrollToAndActivateDate(randomDate)
   }
 
   scrollToAndActivateDate (val) {
     const { state, c } = this
     const { data } = state
     const singles = data ? data.singles : []
-    const anteriorSingles = singles.filter(s => s.date.valueOf() <= val)
-      .sort((s1, s2) => s1.date.valueOf() < s2.date.valueOf())
+    const anteriorSingles = singles
+      .filter(s => s.date.valueOf() <= val)
+      .sort((s1, s2) => s1.date.valueOf() - s2.date.valueOf())
     if (!anteriorSingles.length) {
-      document.querySelector(`.${c}__chapter:first-child`)
-        .scrollIntoView({
-          block: 'start',
-          behavior: 'smooth',
-          inline: 'start'
-        })
+      const $firstChapter = document.querySelector(`.${c}__chapter:first-child`)
+      const browserHeight = Math.max(window.innerHeight, window.innerHeight || 0)
+      const offsetY = $firstChapter.offsetTop - browserHeight / 6
+      window.scroll({
+        top: offsetY,
+        left: 0,
+        behavior: 'smooth'
+      })
       return this.setState({
         selected_date: val,
         active_single_id: null
@@ -232,14 +308,52 @@ export default class LibeLaBowie extends Component {
     const latestSingle = anteriorSingles[anteriorSingles.length - 1]
     const { id: latestSingleId } = latestSingle
     const $latestSingle = document.querySelector(`.${c}__item[data-single-id="${latestSingleId}"]`)
-    $latestSingle.querySelector(`.${c}__item-label`).scrollIntoView({
-      block: 'start',
-      behavior: 'smooth',
-      inline: 'start'
+    const $latestSingleContainer = this.$getClosestParent($latestSingle, `.${c}__items`)
+    const browserHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    const browserWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+    const eltHeight = $latestSingle.offsetHeight
+    const eltWidth = $latestSingle.offsetWidth
+    const offsetX = $latestSingle.offsetLeft
+    const offsetY = $latestSingle.offsetTop + (eltHeight / 2) - (browserHeight / 6)
+    window.scroll({
+      top: offsetY,
+      left: 0,
+      behavior: 'smooth'
     })
-    return setTimeout(() => this.setState({
+    $latestSingleContainer.scroll({
+      top: 0,
+      left: offsetX,
+      behavior: 'smooth'
+    })
+    return this.setState({
       selected_date: val,
       active_single_id: latestSingleId
-    }), 1000)
+    })
+  }
+
+  // Copy pasted this from stackOverflow...
+  $getClosestParent ( elem, selector ) {
+    if (!Element.prototype.matches) {
+      Element.prototype.matches =
+        Element.prototype.matchesSelector ||
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.oMatchesSelector ||
+        Element.prototype.webkitMatchesSelector ||
+        function (s) {
+          const matches = (
+            this.document ||
+            this.ownerDocument
+          ).querySelectorAll(s)
+          let i = matches.length
+          while (--i >= 0 && matches.item(i) !== this) {}
+          return i > -1
+        }
+    }
+    // Get closest match
+    for ( ; elem && elem !== document; elem = elem.parentNode ) {
+      if (elem.matches(selector)) return elem
+    }
+    return null
   }
 }
